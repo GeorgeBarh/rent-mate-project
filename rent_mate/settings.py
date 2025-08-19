2964fa3f57b2dcf22b7d0f0ce850e8631735d6e6
GeorgeBarh <georgebarhabas@gmail.com>
2025-08-20 00:29:30 +0200

    debug change

--- rent_mate/settings.py
+++ rent_mate/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
 ALLOWED_HOSTS = [
