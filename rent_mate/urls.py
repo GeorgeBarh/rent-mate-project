8357a31ad68fd92bbf7d35a3f777c57e27881cde
GeorgeBarh <georgebarhabas@gmail.com>
2025-08-20 01:00:30 +0200

    add meta tags/ improve the SEO and robots.txt

--- rent_mate/urls.py
+++ rent_mate/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from home import views as home_views
+    from django.views.generic import TemplateView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,6 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('products/', include('products.urls')),
     path('rentals/', include('rentals.urls')),
+    path("robots.txt", TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
 ]
+     
